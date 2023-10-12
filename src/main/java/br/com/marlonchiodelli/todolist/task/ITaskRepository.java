2208b9cdba58af1d1df61eb0e89a0ebdb4b18799
MarlonChi <marlonchiodelli@hotmail.com>
2023-10-11 21:00:34 -0300

    feat: aula 3

---
+++ src/main/java/br/com/marlonchiodelli/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.marlonchiodelli.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+}
